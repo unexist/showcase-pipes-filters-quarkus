4817cbecfd2e583ce1a5d3da30d5cb75494d7f37
Christoph Kappel <christoph@unexist.dev>
2021-08-12 10:54:11 +0200

    Added base class for ObjectMapper

---
+++ src/main/java/dev/unexist/showcase/todo/infrastructure/base/AbstractBaseMapper.java
@@ -0,0 +1,35 @@
+/**
+ * @package Showcase
+ *
+ * @file Base class for objectmapper
+ * @copyright 2021 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+ 
+package dev.unexist.showcase.todo.infrastructure.base;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public abstract class AbstractBaseMapper {
+    protected final ObjectMapper mapper;
+
+    /**
+     * Constructor
+     **/
+
+    AbstractBaseMapper() {
+        this.mapper = new ObjectMapper();
+
+        mapper.registerModule(new JavaTimeModule())
+                .registerModule(io.cloudevents.jackson.JsonFormat.getCloudEventJacksonModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    }
+}
